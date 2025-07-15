d5bd708f9972cdd6db6ceb75182cee9f029f251a
Abdelrahman Alaa <Abdelrrahman.Alaa@gmail.com>
2025-07-15 16:59:53 +0300

    Update Appwrite.js to use dynamic endpoint from environment variable
    - Changed the Appwrite endpoint from a hardcoded URL to use the APPWRITE_ENDPOINT variable for better flexibility and configuration management.

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,11 +1,11 @@
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
-// const APPWRITE_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const APPWRITE_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
 import { Client, Databases, ID, Query } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
+  .setEndpoint(APPWRITE_ENDPOINT)
   .setProject(PROJECT_ID);
 
 const database = new Databases(client);
